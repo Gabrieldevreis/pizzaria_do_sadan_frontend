21060b265bf867ebefec6ce525ca49ae268e097d
gabrieldevreis <gabrielreisdev@gmail.com>
2026-07-14 19:39:59 -0300

    Enhance type definitions by adding Pedido interface and TipoEntrega type. Updated Produto interface for clarity and consistency.

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,19 @@
 export interface Produto {
-    id: number;
-    nome: string;
-    categoria: 'salgada' | 'doce' | 'bebida' | 'fogaca' | 'esfirra'; // Restringimos para aceitar apenas essas categorias
-    preco: number;
-    descricao: string;
-  }
+  id: number;
+  nome: string;
+  categoria: 'salgada' | 'doce' | 'bebida' | 'fogaca' | 'esfirra';
+  preco: number;
+  descricao: string;
+}
+
+export type TipoEntrega = 'mesa' | 'retirada';
+
+export interface Pedido {
+  id: number;
+  data: string;
+  nome: string;
+  valor: number;
+  tipoEntrega: TipoEntrega;
+  mesa?: number;
+  itens: Produto[];
+}
